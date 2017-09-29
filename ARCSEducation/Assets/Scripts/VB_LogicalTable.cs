d0d07ff4a279a76d46178b246961c9ebdeef2bea
Hamilton Chevez <hchev001@fiu.edu>
2017-09-29 01:33:32 -0400

    finished logical OR

--- ARCSEducation/Assets/Scripts/VB_LogicalTable.cs
+++ ARCSEducation/Assets/Scripts/VB_LogicalTable.cs
@@ -6,11 +6,20 @@ using Vuforia;
 public class VB_LogicalTable : MonoBehaviour, IVirtualButtonEventHandler {
 
 	private GameObject VButtonObject;
+	private GameObject table;
+
+	void Awake () {
+		
+	}
 
 	// Use this for initialization
 	void Start () {
 		VButtonObject = GameObject.Find ("ORBooleanTableButton");
+		table = GameObject.Find ("LogicalORTableSketch");
+
 		VButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+		table.GetComponent<MeshRenderer> ().enabled = false;
+
 	}
 	
 	// Update is called once per frame
@@ -19,11 +28,13 @@ public class VB_LogicalTable : MonoBehaviour, IVirtualButtonEventHandler {
 	}
 
 	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb) {
-	
+		
 		Debug.Log ("Button down");
+		table.GetComponent<MeshRenderer> ().enabled = true;
 	}
 
 	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb) {
 		Debug.Log ("Button up");
+		table.GetComponent<MeshRenderer> ().enabled = false;
 	}
 }
