5284b1308cfe7167efb8418e780e8cab21ab9fb2
Hamilton Chevez <hchev001@fiu.edu>
2017-09-29 00:41:26 -0400

    working on Logical OR model

---
+++ ARCSEducation/Assets/Scripts/VB_LogicalTable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VB_LogicalTable : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject VButtonObject;
+
+	// Use this for initialization
+	void Start () {
+		VButtonObject = GameObject.Find ("ORBooleanTableButton");
+		VButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb) {
+	
+		Debug.Log ("Button down");
+	}
+
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb) {
+		Debug.Log ("Button up");
+	}
+}
